59350811e6701b4a2e658210b64800de99b38314
moradalkhatib <muradalkhateb@icloud.com>
2021-10-03 12:41:00 +0300

    morad

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Image, Text, Button } from "react-native";
+import {
+  SafeAreaView,
+  withSafeAreaInsets,
+} from "react-native-safe-area-context";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={require("./assets/tenor.png")} />
+      <Button
+        style={styles.buttonStyle}
+        title="Buy Your Smile With One Dollar"
+      ></Button>
       <StatusBar style="auto" />
-    </View>
+      <Text style={styles.textStyle}>A Joke For Dollar</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    color: "white",
+    fontSize: 20,
+  },
+  buttonStyle: {
+    backgroundColor: "white",
   },
 });
